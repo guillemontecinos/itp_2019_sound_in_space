4fa44ea2dd75b0244c288508d827a1d6f9669981
gamontec <gamontec@uc.cl>
2019-03-05 09:07:21 -0500

    mono progress

--- 01_monophonic/tone_sketches/sketch2.js
+++ 01_monophonic/tone_sketches/sketch2.js
@@ -13,10 +13,16 @@ Tone.Transport.start(0)
 
 const scale = [33, 51, 73, 93, 115, 140, 155]
 
-const seq = new Tone.Sequence(function(time, step){
-	// console.log(note);
+const seq1 = new Tone.Sequence(function(time, step){
+	console.log(scale[step+3]);
   poly.triggerAttackRelease(scale[step+3], "4n")
 //straight quater notes
-}, [0, 1, 2, 3], "4n");
+}, [0, 1, 2, 3], "4n")
 
-seq.start(0)
+const seq2 = new Tone.Sequence(function(time, step){
+	console.log("bass")
+	poly.triggerAttackRelease(scale[0], "3n")
+}, [0], "1n")
+
+seq1.start(0)
+seq2.start(0)
